73bee0d3cd86083d8769dddb5dc78b919b1d5851
Valerio Salzano <vlr.slzn@gmail.com>
2023-03-08 15:30:41 +0100

    bonus 1

--- js/main.js
+++ js/main.js
@@ -30,22 +30,46 @@ for (let i=0; i<teamMembersNumber; i++){
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
 
-const teamDataContainerDom = document.getElementById('teamData-container');
+/*
+    const teamDataContainerDom = document.getElementById('teamData-container');
 
-for (let i=0; i<teamMembersNumber; i++){
-    const teamListRow = document.createElement('p');
-    for (let key in teamMembers[i]){
-        const teamListCol = document.createElement('span');
-        teamListCol.innerHTML = (`${key} : ${teamMembers[i][key]}; <br>`);
-        teamListRow.append(teamListCol);
+    for (let i=0; i<teamMembersNumber; i++){
+        const teamListRow = document.createElement('p');
+        for (let key in teamMembers[i]){
+            const teamListCol = document.createElement('span');
+            teamListCol.innerHTML = (`${key} : ${teamMembers[i][key]}; <br>`);
+            teamListRow.append(teamListCol);
+        }
+        teamDataContainerDom.append(teamListRow);
     }
-    teamDataContainerDom.append(teamListRow);
-}
+*/
 
 // BONUS 1:
 // Trasformare la stringa foto in una immagine effettiva
 
+const teamDataContainerDom = document.getElementById('teamData-container');
 
+// scorro i membri (object) del team
+for (let i=0; i<teamMembersNumber; i++){
+    // ogni membro sarà descritto in un "p"
+    const teamListRow = document.createElement('p');
+    // scorro gli attributi (key) dei membri del team 
+    for (let key in teamMembers[i]){
+        const teamListCol = document.createElement('span');
+        // verifico se sto esaminando la path dell'immagine o una stringa
+        if (key == 'imgPath'){
+            // CASO 1: path immagine 
+            const picture = document.createElement('img');
+            picture.src = `img/${teamMembers[i][key]}`;
+            teamListCol.append(picture);
+        } else {
+            // CASO 2: stringa
+            teamListCol.innerHTML = (`${key} : ${teamMembers[i][key]}; <br>`);
+        }
+        teamListRow.append(teamListCol);
+    }
+    teamDataContainerDom.append(teamListRow);
+}
 
 // BONUS 2:
 // Organizzare i singoli membri in card/schede
